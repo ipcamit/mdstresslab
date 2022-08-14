e622d0eaf6143ac3b711c564f2f01c46a7e82b2f
Amit Gupta <amit_@outlook.in>
2022-08-13 19:28:01 -0500

    checkcpoint before debugging

--- py_modules.cpp
+++ py_modules.cpp
@@ -7,6 +7,12 @@
 
 #include "BoxConfiguration.h"
 #include "kim.h"
+#include "Grid.h"
+#include "Sphere.h"
+#include "Stress.h"
+#include "calculateStress.h"
+#include "SubConfiguration.h"
+#include "Stencil.h"
 
 namespace py = pybind11;
 
@@ -23,4 +29,37 @@ PYBIND11_MODULE(pymdstresslab, m){
     py::class_<Kim>(m, "Kim")
             .def(py::init<>())
             .def(py::init<std::string>());
+
+    py::class_<GridBase>(m, "GridBase")
+            .def(py::init<>());
+
+    py::class_<Grid<Current>,GridBase>(m, "GridCurrent")
+            .def(py::init<int>())
+            .def(py::init<Vector3d, Vector3d, int>())
+            .def("read",&Grid<Current>::read)
+            .def("read",&Grid<Current>::write);
+
+    py::class_<Grid<Reference>,GridBase>(m, "GridReference")
+            .def(py::init<int>())
+            .def(py::init<Vector3d, Vector3d, int>())
+            .def("read",&Grid<Reference>::read)
+            .def("read",&Grid<Reference>::write);
+
+    py::class_<Sphere>(m, "Sphere")
+            .def(py::init<double>())
+            .def(py::init<Sphere&>());
+
+    py::class_<Stress<Sphere,Cauchy> >(m, "StressCauchy")
+            .def(py::init<std::string,Sphere &, Grid<Current>* >());
+    py::class_<Stress<Sphere,Piola> >(m, "StressPiola")
+            .def(py::init<std::string,Sphere &, Grid<Reference>* >());
+
+    py::class_<Stencil>(m, "Stencil")
+            .def(py::init<Configuration&>());
+    py::class_<SubConfiguration,Configuration>(m, "SubConfiguration")
+            .def(py::init<Stencil &>());
+
+    m.def("calculateStressCauchy",[](BoxConfiguration & body, Kim & kim, Stress<Sphere,Cauchy>& stress){
+        calculateStress(body, kim, std::tie(stress));
+    });
 }
--- unit_tests/testSW/testSW.py
+++ unit_tests/testSW/testSW.py
@@ -1,8 +1,42 @@
-import pymdstresslab
+import pymdstresslab as pmsl
+import numpy as np
 
 model_name = "SW_StillingerWeber_1985_Si__MO_405512056662_005"
 
-box = pymdstresslab.BoxConfiguration(8000,1)
-box.read("config.data",1)
+body = pmsl.BoxConfiguration(8000,1)
+body.read("config.data",1)
 
-kim = pymdstresslab.Kim(model_name)
+kim = pmsl.Kim(model_name)
+
+vec1 = np.array([0., 0., 0.])
+vec2 = np.array([60., 60., 60.])
+
+ngrid = 100
+randomGrid = pmsl.GridCurrent(vec1, vec2, ngrid)
+referenceRandomGrid = pmsl.GridReference(vec1, vec2, ngrid)
+
+ngrid = 20
+reference_grid = pmsl.GridReference(vec1, vec2, ngrid)
+
+ngrid = 125
+gridFromFile = pmsl.GridCurrent(ngrid)
+gridFromFile.read("grid_cauchy.data")
+
+hardy1 = pmsl.Sphere(5.29216036151419)
+hardyStress1 = pmsl.StressCauchy("hardy1", hardy1, gridFromFile)
+
+hardy2 = pmsl.Sphere(20.)
+hardyStress2 = pmsl.StressCauchy("hardy2", hardy2, gridFromFile)
+
+hardy3 = pmsl.Sphere(5.)
+hardyStress3 = pmsl.StressPiola("hardy3",hardy3, reference_grid)
+
+hardy4 = pmsl.Sphere(7.)
+hardyStress4 = pmsl.StressPiola("hardy4",hardy4, reference_grid)
+
+hardyRandom = pmsl.Sphere(9.)
+hardyStressRandomCauchy = pmsl.StressCauchy("hardyRandomCauchy", hardyRandom, randomGrid)
+
+hardyStressRandomPiola = pmsl.StressPiola("hardyRandomPiola", hardyRandom, referenceRandomGrid)
+
+pmsl.calculateStressCauchy(body, kim, hardyStressRandomCauchy)
